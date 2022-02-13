64eccc06225272714dc40cb3a004560b708760ab
Elmer Menjivar <elmermenjivar48@gmail.com>
2022-02-13 10:32:46 -0600

    Agregando Ejercicio 16

---
+++ Ejercicios/Ejercicio16 - Ciclos While/CiclosWhile.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    int a = 5;
+    int b = 2;
+
+    while(a > b ){
+        cout << "Ingrese el valor de a " << endl;
+        cin >> a;
+
+    }
+
+    cout << endl;
+    cout << "Salistes del cliclo!";
+
+
+    return 0;
+}
