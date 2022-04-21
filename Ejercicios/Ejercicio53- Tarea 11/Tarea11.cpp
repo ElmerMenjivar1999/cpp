42f0b9322b050d83678a4d715da711e1e7148da0
Elmer Menjivar <elmermenjivar48@gmail.com>
2022-04-20 18:26:47 -0600

    modificando

--- Ejercicios/Ejercicio53- Tarea 11/Tarea11.cpp
+++ Ejercicios/Ejercicio53- Tarea 11/Tarea11.cpp
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[])
     cout << endl;
     cout << "Mostrando los Elementos del arreglo bidimensional" << endl;
     cout << endl;
-    
+
     for(int i = 0; i < 5; i++){
         
         for(int j = 0; j < 5; j++){
@@ -27,7 +27,7 @@ int main(int argc, char const *argv[])
         cout << "\n";
     }
     cout << endl;
-    cout << "La suma total del los elmenetos del arreglo bidimensional es: " << sumaTotal << endl;
+    cout << "La suma total del los elementos del arreglo bidimensional es: " << sumaTotal << endl;
     cout << endl;
 
     return 0;
